ebf019cc7f4888f947f750667bd3cccc4e461196
ibk01161 <rkxmsdk@naver.com>
2022-08-22 23:46:17 +0900

    Add Part2_17

---
+++ PART2/examples/056_app.js
@@ -0,0 +1,4 @@
+// hello.js의 hello 함수를 import 키워드를 이용하여 가지고 옴
+import { hello } from "/056_hello";
+
+hello('es6 module');
---
+++ PART2/examples/056_hello.js
@@ -0,0 +1,4 @@
+// hello 함수를 정의하고 이 함수를 export 키워드를 이용하여 외부에서 가져올 수 있게 내보냈음
+export function hello(name) {
+    console.log(`hello ${name}`);
+}
