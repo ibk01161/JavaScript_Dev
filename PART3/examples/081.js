ca2b5ef542934176bc86acfcae1fe6fa8273fb96
ibk01161 <rkxmsdk@naver.com>
2022-08-30 23:29:49 +0900

    Add Part3_7

---
+++ PART3/examples/081.js
@@ -0,0 +1,14 @@
+// 배열 뒤에 요소 추가하기 (push)
+const festa = ['mang'];
+
+// 문장 순서대로 festa 배열 뒤에 요소를 추가
+festa.push('chimmy');
+festa.push('tata');
+festa.push('cooky');
+festa.push('shooky');
+festa.push('koya');
+festa.push('rj');
+
+festa.forEach(name => {
+    console.log(name);
+});
